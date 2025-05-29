f627b2570dc625295db62f3bfdc7c72a377ba539
Seogo <77395226+ksh6940@users.noreply.github.com>
2025-05-29 10:18:57 +0900

    Update main.py

--- Examples/ping-pong game/main.py
+++ Examples/ping-pong game/main.py
@@ -49,3 +49,64 @@ computer = pygame.Rect(SCREEN_WIDTH // 2,  40, 50, 15)
 
 player_score = 0
 computer_score = 0
+
+# 공 리셋 함수
+def reset_ball(center_x, center_y):
+    global ball, ball_dx, ball_dy
+    ball.x = center_x
+    ball.y = center_y
+    ball_dx = random.choice([-3, -2, -1, 1, 2, 3])
+    ball_dy = 5
+
+# 게임 리셋 함수
+def reset_score():
+    global player_score, computer_score
+    player_score = 0
+    computer_score = 0
+
+# 이벤트 함수
+def event():
+    global player_dx
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_dx = -5
+            elif event.key == pygame.K_RIGHT:
+                player_dx = 5
+        if event.type == pygame.KEYUP:
+            if event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
+                player_dx = 0
+    return False
+
+# 게임 업데이트 함수
+def update_game():
+    global ball, ball_dx, ball_dy, player, player_dx, player_score, computer_score, computer
+
+    # 공 이동
+    ball.x = ball_dx + ball.x
+    ball.y = ball_dy + ball.y
+
+    # 공이 충돌한 경우
+    if ball.left < 0 or ball.right > SCREEN_WIDTH:
+        ball_dx = ball_dx * (-1)
+
+    # 플레이어 이동 
+    player.x = player.x + player_dx
+
+    # 플레이어가 화면 밖으로 이동한 경우
+    if player.left < 0:
+        player.left = 0 # player_dx = 0
+    if player.right > SCREEN_WIDTH:
+        player.right = SCREEN_WIDTH # player_dx = 0
+    
+    # 컴퓨터(적) 이동
+    if computer.centerx > ball.centerx:
+        computer.x = computer.x - 3
+    if computer.centerx < ball.centerx:
+        computer.x = computer.x + 3
+    
+    # 공이 플레이어/컴퓨터에 부딪혔을 때
+    if ball.colliderect
